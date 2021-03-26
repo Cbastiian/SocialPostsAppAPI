1db4a710767bb5e8688cda893b52998bdb0a53df
Cbastiian <sebastiancordoba11999@gmail.com>
2021-03-25 22:31:38 -0500

    Configuracion inicial de JWT para futura autenticacion

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Packages
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        
         //Api
         App\Providers\Api\TestProvider::class
 
