959356a0e1f49a314b53a849765c409a46025cc9
Sebastian Cordoba <sebastiancordoba11999@gmail.com>
2021-04-28 21:08:31 -0500

    Configuracion para semilla de motivo de reportes

--- database/seeders/ReportReasesonsSeeder.php
+++ database/seeders/ReportReasesonsSeeder.php
@@ -3,11 +3,23 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class ReportReasesonsSeeder extends Seeder
 {
     public function run()
     {
-        //
+        DB::table('report_reasons')->truncate();
+        $json = File::get('database/data/report_reasons.json');
+        $data = json_decode($json);
+
+        foreach ($data as $obj) {
+            DB::table('report_reasons')->insert([
+                'name' => $obj->name,
+                'description' => $obj->description,
+                'code' => $obj->code
+            ]);
+        }
     }
 }
